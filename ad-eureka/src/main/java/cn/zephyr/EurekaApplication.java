dddefc1209cbc4c37b801f91b95acb2757e604fb
zephyrlai <laizonghao@foxmail.com>
2019-09-24 17:34:22 +0800

    first commit

---
+++ ad-eureka/src/main/java/cn/zephyr/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.zephyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName: EurekaApplication
+ * @Author: laizonghao
+ * @Description:
+ * @Date: 2019/9/24 17:27
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
