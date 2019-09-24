ae2c1dac25638e41bc8613bcbebfc137db72d0d8
zephyrlai <laizonghao@foxmail.com>
2019-09-24 19:29:27 +0800

    新增全局消息转化器

---
+++ zephyr-ad-service/ad-common/src/main/java/cn/zephyr/config/WebConfiguration.java
@@ -0,0 +1,21 @@
+package cn.zephyr.config;
+
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @ClassName: WebConfiguration
+ * @Author: laizonghao
+ * @Description:
+ * @Date: 2019/9/24 19:23
+ */
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+}
